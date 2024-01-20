ce1544031d0f92c69a4dc2f14c69ff8475cae160
Michael Scholten <michael-scholten@hotmail.nl>
2024-01-20 20:21:36 +0100

    Made the window work in the browser

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,49 @@
 #![warn(clippy::pedantic, clippy::nursery)]
 
 use winit::{
-    error::OsError,
     event::{ElementState, KeyboardInput, VirtualKeyCode},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
 
-/// # Errors
-/// Returns an error if the window couldn't be created
-pub fn run() -> Result<(), OsError> {
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+/// # Panics
+/// panics if the window couldn't be created
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn run() {
+    // Required for wgpu error messages to be printed
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch="wasm32")]{
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+            console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
+        }else{
+            env_logger::init();
+        }
+    }
+
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop)?;
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        // Winit prevents sizing with CSS, so we have to set the size manually when on the web.
+        use winit::dpi::PhysicalSize;
+        use winit::platform::web::WindowExtWebSys;
+
+        window.set_inner_size(PhysicalSize::new(450, 400));
+
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| {
+                let dst = doc.get_element_by_id("wasm-example")?;
+                let canvas = web_sys::Element::from(window.canvas());
+                dst.append_child(&canvas).ok()?;
+                Some(())
+            })
+            .expect("Couldn't append canvas to document body.");
+    }
 
     event_loop.run(move |event, _, control_flow| match event {
         winit::event::Event::WindowEvent { window_id, event } if window_id == window.id() => {
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use learn_wgpu::run;
 
 fn main() {
-    // Required for wgpu error messages to be printed
-    env_logger::init();
-
-    run().unwrap();
+    run();
 }
